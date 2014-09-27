988ebb16854889725fa16dd2cb595b7c43d4f9ea
iGeesus <Mithrandir@Jesuss-MacBook-Pro.local>
2014-09-27 12:01:54 -0500

    Initial Commit

---
+++ UITableView Challenge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UITableView Challenge
+//
+//  Created by Jesús Santa Olalla on 9/27/14.
+//  Copyright (c) 2014 Jesús Santa Olalla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UITableView Challenge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UITableView Challenge
+//
+//  Created by Jesús Santa Olalla on 9/27/14.
+//  Copyright (c) 2014 Jesús Santa Olalla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
